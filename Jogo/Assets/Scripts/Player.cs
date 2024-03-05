b434f8c5297b2887a520210c7ebc88f329986e94
Gustavo Fialho <gustavosalgueirofialho@gmail.com>
2024-03-05 21:57:38 +0000

    Projeto
    Primeiro 'commit' do projeto. Foram realizados alguns testes de funcionamento no Unity.

---
+++ Jogo/Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float Speed;
+    public float JumpForce;
+    public bool isJumping;
+    public bool doubleJump;
+    private Rigidbody2D rig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig=GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        Vector3 movement=new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position+=movement*Time.deltaTime*Speed;
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            if(!isJumping)
+            {
+                rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                doubleJump=true;
+            }
+            else
+            {
+                if(doubleJump)
+                {
+                    rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                    doubleJump=false;
+                }
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col_ground)
+    {
+        if(col_ground.gameObject.layer == 8) //8 é o número da layer em que foi posta a camada do 'chão'
+        {
+            isJumping=false;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col_ground)
+    {
+        if(col_ground.gameObject.layer == 8) //8 é o número da layer em que foi posta a camada do 'chão'
+        {
+            isJumping=true;
+        }
+    }
+}
