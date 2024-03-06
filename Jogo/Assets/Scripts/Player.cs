2876d70d63b8289b8daca992415b4e763a57224e
Gustavo Fialho <gustavosalgueirofialho@gmail.com>
2024-03-06 19:38:05 +0000

    Animações (Parte 1)
    Adicionada animação de andar, saltar e estar parado (para o eixo x+)

--- Jogo/Assets/Scripts/Player.cs
+++ Jogo/Assets/Scripts/Player.cs
@@ -9,10 +9,12 @@ public class NewBehaviourScript : MonoBehaviour
     public bool isJumping;
     public bool doubleJump;
     private Rigidbody2D rig;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        rig=GetComponent<Rigidbody2D>();
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,8 +26,10 @@ public class NewBehaviourScript : MonoBehaviour
 
     void Move()
     {
-        Vector3 movement=new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position+=movement*Time.deltaTime*Speed;
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * Speed;
+        if(Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Horizontal") < 0) anim.SetBool("walk", true);
+        else anim.SetBool("walk", false);
     }
 
     void Jump()
@@ -35,14 +39,14 @@ public class NewBehaviourScript : MonoBehaviour
             if(!isJumping)
             {
                 rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
-                doubleJump=true;
+                doubleJump = true;
             }
             else
             {
                 if(doubleJump)
                 {
                     rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
-                    doubleJump=false;
+                    doubleJump = false;
                 }
             }
         }
@@ -52,7 +56,7 @@ public class NewBehaviourScript : MonoBehaviour
     {
         if(col_ground.gameObject.layer == 8) //8 é o número da layer em que foi posta a camada do 'chão'
         {
-            isJumping=false;
+            isJumping = false;
         }
     }
 
@@ -60,7 +64,7 @@ public class NewBehaviourScript : MonoBehaviour
     {
         if(col_ground.gameObject.layer == 8) //8 é o número da layer em que foi posta a camada do 'chão'
         {
-            isJumping=true;
+            isJumping = true;
         }
     }
 }
