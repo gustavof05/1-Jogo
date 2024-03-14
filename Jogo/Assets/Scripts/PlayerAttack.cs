1bcd0a37d2e20a71c1c8e18af898a9b4fe3cea01
Gustavo Fialho <gustavosalgueirofialho@gmail.com>
2024-03-14 14:07:19 +0000

    Tentativa1 de ataque
    Primeira tentativa de ataque. Não funciona a animação (pelo menos). O ataque em si não sei se funciona, é preciso pôr um inimigo com vida a levar dano...

--- Jogo/Assets/Scripts/PlayerAttack.cs
+++ Jogo/Assets/Scripts/PlayerAttack.cs
@@ -6,20 +6,41 @@ public class PlayerAttack : MonoBehaviour
 {
     private float timeforAttack;    //Cooldown de cada ataque
     public float starttimeforAttack;
+    public Transform attackpos; //Posição de "ataque"
+    public float attackrange;   //Range de ataque
+    public int damage;    //Dano tirado por ataque
+    public LayerMask inimigos;  //Camada onde estão os inimigos (para só "acertar" neles)
+    public Animator anim;
+    
     // Update is called once per frame
     void Update()
     {
-        if(timeforAttack <= 0)  //É possível atacar
+        Attack();
+    }
+
+    void Attack()
+    {
+        if(timeforAttack <= 0)  //É possível atacar (cooldown=0)
         {
             if(Input.GetMouseButtonDown(0))
             {
-                
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackpos.position, attackrange, inimigos);   //Nº de inimigos (cada entidade) no "círculo"
+                for(int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                } 
             }
-            timeforAttack = starttimeforAttack;
+            timeforAttack = starttimeforAttack; //Recomeça a contar o cooldown
         }
         else
         {
             timeforAttack -= Time.deltaTime;
         }
     }
+
+    /*void void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackpos.position, attackrange);
+    }*/
 }
