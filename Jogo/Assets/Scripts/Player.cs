944554d9224ac7ed5e80bc9c0e3c67e80ae51dd0
TiagoRegufe <tiago310703@gmail.com>
2024-04-06 19:43:31 +0100

    problemas resolvidos #2
    problemas resolvidos parte 2

--- Jogo/Assets/Scripts/Player.cs
+++ Jogo/Assets/Scripts/Player.cs
@@ -1,47 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
-using JetBrains.Annotations;
 using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.Rendering;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
     public float Speed;
     public float JumpForce;
     public bool isJumping;
     public bool doubleJump;
+    public bool isFalling;
     private Rigidbody2D rig;
     private Animator anim;
-    
-    public GameObject tagging; 
-    public GameObject tagging2; 
-    public GameObject Levels;
-    public float delay = 1f;
-    
-    [SerializeField]
-    private GameObject _object;
-
-
+    public PlayerHurt_Death deathcontroller;
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        
-        // Desativar OptionsPanel no início
-        tagging2.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!pauseMenu.ispaused)
+        if(!deathcontroller.isDead) //Se não estiver morto
         {
             Move();
             Jump();
+            UpdateAnimations();
         }
     }
 
@@ -60,6 +45,8 @@ public class NewBehaviourScript : MonoBehaviour
             transform.eulerAngles = new Vector3(0f, 180f, 0f);  //Mexer no eixo posicional do player
         }
         else anim.SetBool("walk", false);
+        if (rig.velocity.y < 0) isFalling = true;   //Se velocidade em y for negativa, está a cair
+        else isFalling = false;
     }
 
     void Jump()
@@ -76,7 +63,7 @@ public class NewBehaviourScript : MonoBehaviour
             {
                 if(doubleJump)  //Se ativar o Double Jump
                 {
-                    rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);  //Dar o Double Jump
+                    rig.AddForce(new Vector2(0f, JumpForce/3), ForceMode2D.Impulse);  //Dar o Double Jump
                     //anim.SetBool("jump", true); /*NÃO FUNCIONA*/
                     doubleJump = false; //Não é permitido dar (outra vez) Double Jump (opção bloqueada)
                 }
@@ -89,43 +76,9 @@ public class NewBehaviourScript : MonoBehaviour
         if(col_ground.gameObject.layer == 8) //8 é o número da layer em que foi posta a camada do 'chão'
         {
             isJumping = false;  //Para de saltar
+            isFalling = false;
             anim.SetBool("jump", false);
-        }
-        if(col_ground.gameObject.name == "Start")
-        {
-            tagging.SetActive(false);
-            Invoke("delayFunc",delay);
-            
-        }
-        if(col_ground.gameObject.name == "Options")
-        {
-            tagging.SetActive(false);
-            tagging2.SetActive(true);
-            Debug.Log("Ativando");
-        }
-        if(col_ground.gameObject.name == "Quit")
-        {
-            Application.Quit();
-            Debug.Log("Game is exiting");
-        }
-        if(col_ground.gameObject.name == "Cancel")
-        {
-            Levels.SetActive(false);
-            tagging.SetActive(true);
-
-        }
-        if(col_ground.gameObject.name == "Level 1")
-        {
-            SceneManager.LoadScene("LevelOne");
-
-        }
-        if(col_ground.gameObject.name == "Level 2")
-        {
-            SceneManager.LoadScene("LevelTwo");
-        }
-        if(col_ground.gameObject.name == "Level 3")
-        {
-            SceneManager.LoadScene("LevelThree");
+            anim.SetBool("fall", false);
         }
     }
 
@@ -136,9 +89,27 @@ public class NewBehaviourScript : MonoBehaviour
             isJumping = true;   //Começa a saltar
         }
     }
-    void delayFunc()
+
+    void UpdateAnimations()
     {
-        Levels.SetActive(true);
-        Debug.Log("Delay");
+        // Lógica para atualizar as animações com base no estado de salto e queda
+        if (isJumping)  //Se estiver a saltar (sair do ground), verifica se está a cair, ou a saltar
+        {
+            if (isFalling)  //Se estiver a cair
+            {
+                anim.SetBool("jump", false); // Desativa a animação de subida
+                anim.SetBool("fall", true);  // Ativa a animação de queda
+            }
+            else
+            {
+                anim.SetBool("fall", false); // Desativa a animação de queda
+                anim.SetBool("jump", true);  // Ativa a animação de subida
+            }
+        }
+        else
+        {
+            anim.SetBool("jump", false);
+            anim.SetBool("fall", false);
+        }
     }
 }
