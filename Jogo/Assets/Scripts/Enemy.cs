268f12c250a0da54392d165f56fa83ae0ee8d7fd
Gustavo Fialho <gustavosalgueirofialho@gmail.com>
2024-04-08 10:04:29 +0100

    Inimigos
    Começo do trabalho nos inimigos

--- Jogo/Assets/Scripts/Enemy.cs
+++ Jogo/Assets/Scripts/Enemy.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public int health;
-    public float speed;
+    public int ehealth = 13;
+    public float speed = 3;
     private Animator anim;
+    private float c = 0f
     public GameObject bloodEffect;
     // Start is called before the first frame update
     void Start()
@@ -18,14 +19,52 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(health <= 0) Destroy(gameObject);
+        Move();
+        Death();
+    }
+
+    void Move()
+    {
         transform.Translate(Vector2.left * speed * Time.deltaTime);
     }
+
+    /*void OnCollisionEnter2D(Collision2D col_ground) //Ao colidir com o chão
+    {
+        if(col_ground.gameObject.layer == 8) //8 é o número da layer em que foi posta a camada do 'chão'
+        {
+            isJumping = false;  //Para de saltar
+            isFalling = false;
+            anim.SetBool("jump", false);
+            anim.SetBool("fall", false);
+        } 
+    }
+
+    void OnCollisionExit2D(Collision2D col_ground)  //Ao sair do chão
+    {
+        if(col_ground.gameObject.layer == 8) //8 é o número da layer em que foi posta a camada do 'chão'
+        {
+            isJumping = true;   //Começa a saltar
+        }
+    }*/
 
     public void TakeDamage(int damage)
     {
         Instantiate(bloodEffect, transform.position, Quaternion.identity);
-        health -= damage;
+        ehealth -= damage;
         Debug.Log("damage taken");
+    }
+
+    public void Death()
+    {
+        if(ehealth <= 0)
+        {
+            anim.SetTrigger("death");
+            c += Time.deltaTime;
+            if(c == 500f) 
+            {
+                Destroy(gameObject);   //Desaparecimento do inimigo
+                c = 0f;
+            }
+        }
     }
 }
