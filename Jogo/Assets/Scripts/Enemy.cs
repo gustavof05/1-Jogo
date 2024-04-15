4858bff56a365b24571d6dfaf9294a644bfb0db0
Gustavo Fialho <gustavosalgueirofialho@gmail.com>
2024-04-15 09:57:58 +0100

    Sistema de movimento e de ataque
    O inimigo já ataca e já se move. Falta dar um "pause" no movimento normal e parar quando ataca o player

--- Jogo/Assets/Scripts/Enemy.cs
+++ Jogo/Assets/Scripts/Enemy.cs
@@ -12,6 +12,8 @@ public class Enemy : MonoBehaviour
     public LayerMask alvos;  //Camada onde estão os inimigos (para só "acertar" neles)
     public bool EnemyAttacking = false;
     private float timesinceAttack = 0.55f;   //Tempo desde o último ataque (inicial)
+    private float moveTimer = 0f; // Tempo para mover
+    private bool isMovingRight = true; // Flag para indicar direção de movimento
     private int d = 1;  //Ajustar animação
     private Animator anime;
 
@@ -31,20 +33,26 @@ public class Enemy : MonoBehaviour
 
     void Move()
     {
-        //transform.Translate(Vector2.left * speed * Time.deltaTime);
-        //Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        //transform.position += movement * Time.deltaTime * Speed;
-        if(Input.GetAxis("Horizontal") > 0f)
+        if(moveTimer <= 0)
+        {
+            if(isMovingRight) isMovingRight = false; //Inverte a direção de movimento
+            else isMovingRight = true; //Inverte a direção de movimento
+            moveTimer = Random.Range(1f, 3f);   //Inicia o timer de movimento com tempo de espera aleatório entre 1s e 3s
+        }
+        if(isMovingRight)
         {
             anime.SetBool("walk", true);
-            transform.eulerAngles = new Vector3(0f, 0f, 0f);    //Mexer no eixo posicional do player
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
         }
-        else if(Input.GetAxis("Horizontal") < 0f)
+        else
         {
             anime.SetBool("walk", true);
-            transform.eulerAngles = new Vector3(0f, 180f, 0f);  //Mexer no eixo posicional do player
+            transform.Translate(Vector2.left * (-speed) * Time.deltaTime);
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
         }
-        else anime.SetBool("walk", false);
+        //anime.SetBool("walk", false);
+        moveTimer -= Time.deltaTime;
     }
 
     public void TakeDamage(int pdamage)
@@ -57,15 +65,19 @@ public class Enemy : MonoBehaviour
 
     void Attack()
     {
+        timesinceAttack += Time.deltaTime;  //Conta o tempo
         if(timesinceAttack > 0.55f) //É possível atacar após 0.55s do último ataque
         {
             EnemyAttacking = true;
-            anime.SetTrigger("attack"); //"Dispara" o parâmetro attack
-            Collider2D[] playersToDamage = Physics2D.OverlapCircleAll(attackpos.position, attackrange, alvos);   //Nº de inimigos (cada entidade) no "círculo"
-            /*for(int i = 0; playersToDamage.Length; i++)
+            Collider2D[] playersToDamage = Physics2D.OverlapCircleAll(attackpos.position, attackrange, alvos);   //Nº de players no "círculo"
+            if(playersToDamage.Length > 0)
             {
-                playersToDamage[i].GetComponent<Player 1>().TakeDamage(edamage);
-            }*/
+                foreach (Collider2D player in playersToDamage)  //Causa dano aos jogadores dentro do alcance
+                {
+                    anime.SetTrigger("attack"); //"Dispara" o parâmetro attack
+                    player.GetComponent<PlayerHurt_Death>().TakeDamage(edamage);
+                }
+            }
             timesinceAttack = 0.0f;   //Recomeça a contar o tempo
         }
         EnemyAttacking = false;
