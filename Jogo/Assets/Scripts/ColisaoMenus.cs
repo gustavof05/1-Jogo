8aab3131f306c05943e41877feac5e069e6c81a2
TiagoRegufe <tiago310703@gmail.com>
2024-04-29 09:14:53 +0100

    3
    3

--- Jogo/Assets/Scripts/ColisaoMenus.cs
+++ Jogo/Assets/Scripts/ColisaoMenus.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class ColisaoMenus : MonoBehaviour
 {
-    public Player p;
+    public Player pc;
  void OnCollisionEnter2D(Collision2D col_ground) //Ao colidir com o chão
     {
         if(col_ground.gameObject.layer == 7) //7 é o número da layer em que foi posta a camada
         {
-            p.isJumping = false;  //Para de saltar
-            p.isFalling = false;
-            p.anim.SetBool("jump", false);
-            p.anim.SetBool("fall", false);
+            pc.isJumping = false;  //Para de saltar
+            pc.isFalling = false;
+            pc.anim.SetBool("jump", false);
+            pc.anim.SetBool("fall", false);
         }
     }
         void OnCollisionExit2D(Collision2D col_ground)  //Ao sair do chão
     {
         if(col_ground.gameObject.layer == 7) //7 é o número da layer em que foi posta a camada
         {
-            p.isJumping = true;   //Começa a saltar
+            pc.isJumping = true;   //Começa a saltar
         }
     }
 }
