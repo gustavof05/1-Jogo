052371b61cf207142a1e0236b7d9bb84e9f7a613
Gustavo Fialho <gustavosalgueirofialho@gmail.com>
2024-05-22 18:53:48 +0100

    Correção de bugs 2
    Começo do arranjo do tilemap
    Ajuste de atribuição de pontos
    Problema com a animação de morte dos Players

--- Jogo/Assets/Scripts/PlayerHurt_Death1.cs
+++ Jogo/Assets/Scripts/PlayerHurt_Death1.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class PlayerHurt_Death1 : MonoBehaviour
 {
-    public int dtp = 15;
     private Animator hdanim;
     public bool isDead = false;
     public PlayerAttack1 p1;
@@ -28,14 +27,14 @@ public class PlayerHurt_Death1 : MonoBehaviour
         {
             p1.phealth -= edamage; //Vida do player1 perde o valor do dano do inimigo
             hdanim.SetTrigger("hurt");
-            GameController.instance.totalScore -= dtp;
+            GameController.instance.totalScore -= 15;
             GameController.instance.UpdateScoreText();
         }
     }
 
     void Death()
     {
-        GameController.instance.totalScore = 0;
+        GameController.instance.totalScore -= 300;
         GameController.instance.UpdateScoreText();
         hdanim.SetTrigger("death");
         isDead = true;  //Está morto
